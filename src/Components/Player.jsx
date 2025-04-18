46b6fbc6cb177f54415b801783e57f23359c049d
Jonathan-Agreda <flavioagreda1990@gmail.com>
2025-04-18 02:06:36 -0500

    hook useRef

--- src/Components/Player.jsx
+++ src/Components/Player.jsx
@@ -1,32 +1,28 @@
-import { useState } from "react"
+import { useState, useRef } from "react"
 
 export const Player = () => {
-    const [playerName, setPlayerName] = useState("")
-    const [buttonClicked, setButtonClicked] = useState(false)
+    const inputNameRef = useRef(null)
 
-    const handleChangePlayerName = (e) => {
-        setButtonClicked(false)
-        setPlayerName(e.target.value)
-    }
+    const [playerName, setPlayerName] = useState(null)
 
     const handleButtonClick = () => {
-        setButtonClicked(true)
-        console.log(playerName)
+        setPlayerName(inputNameRef.current.value)
+        inputNameRef.current.value = null
     }
 
     return (
         <section className="text-center mt-6" >
             <h2 className="text-[#913131] mb-5 font-bold text-xl" >
-                Bienvenido al juego, {buttonClicked ? playerName : "¿quién eres aspirante?"}</h2>
+                Bienvenido al juego, {playerName ?? "¿quién eres aspirante?"}</h2>
             <p className="flex justify-center items-center" >
-                <input type="text" className="rounded-s-md border-[1.5px] border-solid border-[#3F2121] 
+                <input
+                    ref={inputNameRef}
+                    type="text" className="rounded-s-md border-[1.5px] border-solid border-[#3F2121] 
                 bg-[#3f21218f] p-1 pl-3 text-[#d1f0ec] focus:outline-none"
-                    onChange={(e) => {
-                        handleChangePlayerName(e)
-                    }} />
+                />
                 <button className="rounded-e-md font-bold cursor-pointer border-[1.5px] border-solid 
                 border-[#3F2121] bg-[#691A1A] px-4 py-[0.25rem] text-[#f2f2f2] hover:bg-[#3c8379] 
-                hover:border-[#3c8379] "
+                hover:border-[#3c8379] active:bg-[#2c5f56] active:border-[#2c5f56] "
                     onClick={handleButtonClick}
                 >Guarda tu nombre</button>
             </p>
