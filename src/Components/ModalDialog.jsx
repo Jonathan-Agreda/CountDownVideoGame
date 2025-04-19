4d9ea86605c12a83fef30173048d8a9b52293fe9
Jonathan-Agreda <flavioagreda1990@gmail.com>
2025-04-19 01:10:30 -0500

    renderizado condicional de Modales

--- src/Components/ModalDialog.jsx
+++ src/Components/ModalDialog.jsx
@@ -1,8 +1,16 @@
 import { forwardRef, useImperativeHandle, useRef } from "react"
 
-const ForwardedModalDialog = ({ resultGame, timeTarget }, ref) => {
-    const secondsText = (timeTarget > 1 ? "segundos" : "segundo")
+const ForwardedModalDialog = ({ timeRemaining, onResetTimeRemaining, timeTarget }, ref) => {
     const dialogRef = useRef()
+
+    const userHasLost = timeRemaining <= 0
+    const timeRemainingFormatted = (timeRemaining / 1000).toFixed(2)
+    const userScore = Math.round((1 - (timeRemaining / (timeTarget * 1000))) * 100)
+    const secondsText = (timeTarget > 1 ? "segundos" : "segundo")
+    const tittleModal = userHasLost ? <>¡Has perdido!</> : <>Puntaje total: <strong>{userScore}</strong>/100</>
+    const timerIsStoppedText = userHasLost ? <>No has conseguido parar el tiempo antes del fin de la cuenta atrás. ¡Vuelve a probar suerte!</>
+        : <>Has conseguido parar el tiempo a falta de <strong>{timeRemainingFormatted}</strong> segundos</>
+
     useImperativeHandle(ref, () => ({
         open: () => {
             dialogRef.current.showModal()
@@ -15,11 +23,17 @@ const ForwardedModalDialog = ({ resultGame, timeTarget }, ref) => {
                 ref={dialogRef}
                 className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 border-none rounded-lg p-8 bg-[#d7fcf8]">
                 <h2 className="font-mono text-center uppercase font-bold text-4xl mt-2 mb-2">
-                    ¡Has {resultGame}!</h2>
+                    {tittleModal}
+                </h2>
                 <p className="text-lg">
                     El contrareloj esta pautado para finalizar en {timeTarget} {secondsText} </p>
-                <p> Has parado el tiempo a falta de <strong>X</strong> segundos</p>
-                <form action="dialog">
+                <p>
+                    {timerIsStoppedText}
+                </p>
+                <form
+                    action="dialog"
+                    onSubmit={onResetTimeRemaining}
+                >
                     <button
                         className="mt-4 pt-2 pb-2 pl-4 pr-4 border-none
                     rounded-md bg-[#12352f] text-[#edfcfa] text-lg cursor-pointer hover:bg-[#051715] active:bg-[#0d1f1c]"
--- src/Components/Timer.jsx
+++ src/Components/Timer.jsx
@@ -4,37 +4,49 @@ import { ModalDialog } from "./ModalDialog"
 
 
 export const Timer = ({ title, time }) => {
-    const [hasLooser, setHasLooser] = useState(false)
-    const [hasStart, setHasStart] = useState(false)
+    const initialTime = time * 1000
+    const [timeRemaining, setTimeRemaining] = useState(initialTime)
 
     const timerRef = useRef()
     const dialogModalRef = useRef()
 
+    const timerIsRunning = timeRemaining > 0 && timeRemaining < initialTime
+
+    if (timeRemaining <= 0) {
+        clearInterval(timerRef.current)
+        dialogModalRef.current.open()
+    }
+
+    const onResetTimeRemaining = () => {
+        setTimeRemaining(initialTime)
+    }
+
     const handleStartTime = () => {
-        setHasStart(true)
-        timerRef.current = setTimeout(() => {
-            dialogModalRef.current.open()
-            setHasStart(false)
-            setHasLooser(true)
-        }, time * 1000)
+        timerRef.current = setInterval(() => {
+            setTimeRemaining(prevTimeRemaining => prevTimeRemaining - 100)
+        }, 100)
     }
 
     const handleStopTime = () => {
-        setHasStart(false)
         clearInterval(timerRef.current)
+        dialogModalRef.current.open()
     }
 
 
     return (
         <>
-            <ModalDialog ref={dialogModalRef} resultGame="perdido" timeTarget={time} />
+            <ModalDialog
+                ref={dialogModalRef}
+                onResetTimeRemaining={onResetTimeRemaining}
+                timeRemaining={timeRemaining}
+                timeTarget={time}
+            />
             <section
                 className="w-[22rem] flex flex-col items-center justify-center p-8 m-8
         bg-[#691A1A] text-[#edfcfa] shadow-lg rounded-md">
                 <h2
                     className="text-xl tracking-wide text-center uppercase font-bold mb-2"
                 >{title}</h2>
-                {hasLooser && <p className="my-1 mb-3">Has perdido</p>}
                 <p
                     className="border-[1px] border-solid border-[#46cebe] rounded px-2 py-1 m-"
                 >{time} segundo{time > 1 ? "s" : ""}</p>
@@ -42,8 +54,8 @@ export const Timer = ({ title, time }) => {
                     className="mt-4 px-4 py-2 border-none rounded-md bg-[#12352f]
             text-lg cursor-pointer hover:bg-[#051715] active:bg-[#0d1f1c]"
                 >
-                    <button onClick={hasStart ? handleStopTime : handleStartTime}>
-                        {hasStart ? "Parar" : "Empezar"}
+                    <button onClick={timerIsRunning ? handleStopTime : handleStartTime}>
+                        {timerIsRunning ? "Parar" : "Empezar"}
                     </button>
                 </p>
                 <p></p>
